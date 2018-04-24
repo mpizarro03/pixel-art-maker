8347034d7a050c5b619ea8dc05475eed0032dc8a
Melissa Pizarro <mpizarro03@gmail.com>
2018-04-23 18:25:58 -0600

    pixels and palette work with click function

--- pixel.js
+++ pixel.js
@@ -1,34 +1,27 @@
-// <button id="clicker">Boring button</button>
-// <script>
-// // 1. Find and store the element we want to listen to events on.
-// var clickerButton = document.getElementById("clicker");
-// // 2. Define the function that will respond to the event.
-// // callback
-// var onButtonClick = function() {
-//     clickerButton.textContent = "Oh wow, you clicked me!";
-// };
-// // 3. Add the event listener for the element and function
-// // anonymous "inline"
-// clickerButton.addEventListener("click", onButtonClick);
-//
-// clickerButton.addEventListener("click", function() {
-//     clickerButton.textContent = "Oh wow, you clicked me!";
-// });
-//
+
 document.addEventListener("DOMContentLoaded", function() {
+  //canvas grid
   let canvas = document.getElementsByClassName("canvas")[0]
   for(let i = 0; i < 4; i++) {
     let pixel = document.createElement("div");
     pixel.classList.add("pixel");
     canvas.appendChild(pixel);
   }
+  // //click function for pixels on canvas
+  // canvas.addEventListener("click", function(event) {
+  //   console.log(event.target)
+  //   event.target.className = "pixel"
+  // })
 
-  canvas.addEventListener("click", function(event) {
-  console.log(event.target)
-  event.target.className = "pixel"
-  })
+  let palette = document.getElementsByClassName("palette")[0]
+  palette.addEventListener("click", function(event) {
+      console.log(event.target)
+      let currentColor = event.target.className
+      console.log(currentColor)
+      canvas.addEventListener("click", function(event){
+        event.target.className = currentColor
+      })
+    })
 
 
-
-
-});
+  });
